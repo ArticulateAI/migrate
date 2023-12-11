048c6d7c203494e93b84531dcf727e48b050220d
shehan360 <shehan360@icloud.com>
2023-12-11 16:55:34 -0500

    split staging, prod migration scripts

--- update-db.sh
+++ update-db.sh
@@ -5,7 +5,23 @@
 
 mkdir -p sql_data
 
-aws s3 cp s3://docker-container-data/migrations/ ./sql_data/ --recursive
+DEFAULT_ENV_TYPE="staging"
+FOLDER_NAME=${ENV_TYPE:-$DEFAULT_ENV_TYPE}
+if [ -z "${ENV_TYPE}" ]; then
+    echo "ENV_TYPE variable is unset or set to the empty string. Using staging as default..."
+fi
+
+# check if folder name is staging or prod
+if [ "$FOLDER_NAME" = "staging" ]; then
+    echo "using staging migrations"
+elif [ "$FOLDER_NAME" = "prod" ]; then
+    echo "using prod migrations"
+else
+    echo "ENV_TYPE is set to '$FOLDER_NAME'. Please set ENV_TYPE to staging or prod"
+    exit 1
+fi
+
+aws s3 cp "s3://docker-container-data/migrations/${FOLDER_NAME} ./sql_data/" --recursive
 
 version=$1
 if [ -z "$version" ];
